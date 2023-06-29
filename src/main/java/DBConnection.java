7a4289bb734ed608cf5f2cad7504c4c62fb1b135
abhay <abhaysuthar7777@gmail.com>
2023-06-29 22:14:51 +0530

    New Repository

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,21 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+	public static Connection getConnection() {
+		Connection con = null;
+		try{  
+			Class.forName("com.mysql.cj.jdbc.Driver");  
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/eSchool","root","Abhay@3690");
+		}catch (ClassNotFoundException e) {
+			//e.printStackTrace();
+			System.out.println("Not able find required Driver!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
